c6570af735334186dd6c225ddd78fb5b03c92a7c
Davifreitas77 <davifreitaz999@gmail.com>
2026-07-14 20:53:42 -0300

    fix: update icon and adjust gradient colors in Compare component

--- src/components/ui/compare.tsx
+++ src/components/ui/compare.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect, useRef, useCallback } from "react";
  
 import { AnimatePresence, motion } from "motion/react";
 import { cn } from "@/lib/utils";
-import { IconDotsVertical } from "@tabler/icons-react";
+import { IconArrowLeft } from "@tabler/icons-react";
 import { SparklesCore } from "./sparkles";
 
 interface CompareProps {
@@ -167,7 +167,7 @@ export const Compare = ({
     >
       <AnimatePresence initial={false}>
         <motion.div
-          className="h-full w-px absolute top-0 m-auto z-30 bg-gradient-to-b from-transparent from-[5%] to-[95%] via-indigo-500 to-transparent"
+          className="h-full w-px absolute top-0 m-auto z-30 bg-gradient-to-b from-transparent from-[10%] to-[95%] via-[#d4af37] to-transparent"
           style={{
             left: `${sliderXPercent}%`,
             top: "0",
@@ -175,8 +175,8 @@ export const Compare = ({
           }}
           transition={{ duration: 0 }}
         >
-          <div className="w-36 h-full [mask-image:radial-gradient(100px_at_left,white,transparent)] absolute top-1/2 -translate-y-1/2 left-0 bg-gradient-to-r from-indigo-400 via-transparent to-transparent z-20 opacity-50" />
-          <div className="w-10 h-1/2 [mask-image:radial-gradient(50px_at_left,white,transparent)] absolute top-1/2 -translate-y-1/2 left-0 bg-gradient-to-r from-cyan-400 via-transparent to-transparent z-10 opacity-100" />
+          <div className="w-36 h-full [mask-image:radial-gradient(100px_at_left,white,transparent)] absolute top-1/2 -translate-y-1/2 left-0 bg-gradient-to-r from-amber-200 via-transparent to-transparent z-20 opacity-50" />
+          <div className="w-10 h-1/2 [mask-image:radial-gradient(50px_at_left,white,transparent)] absolute top-1/2 -translate-y-1/2 left-0 bg-gradient-to-r from-amber-200 via-transparent to-transparent z-10 opacity-100" />
           <div className="w-10 h-3/4 top-1/2 -translate-y-1/2 absolute -right-10 [mask-image:radial-gradient(100px_at_left,white,transparent)]">
             <MemoizedSparklesCore
               background="transparent"
@@ -189,7 +189,7 @@ export const Compare = ({
           </div>
           {showHandlebar && (
             <div className="h-5 w-5 rounded-md top-1/2 -translate-y-1/2 bg-white z-30 -right-2.5 absolute   flex items-center justify-center shadow-[0px_-1px_0px_0px_#FFFFFF40]">
-              <IconDotsVertical className="h-4 w-4 text-black" />
+              <IconArrowLeft className="h-4 w-4 text-black" />
             </div>
           )}
         </motion.div>
